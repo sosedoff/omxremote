59c8c211626866593f73ece33ad3ea7ee1d427ea
Dan Sosedoff <dan.sosedoff@gmail.com>
2017-08-25 11:12:55 -0500

    Add Zeroconf support to allow omxremote to be discovered on the network

---
+++ zeroconf.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/grandcat/zeroconf"
+)
+
+const (
+	zeroConfName    = "omxremote"
+	zeroconfService = "_http._tcp"
+	zeroconfDomain  = "local."
+	zeroconfPort    = 8080
+)
+
+func startZeroConfAdvertisement(stop chan bool) {
+	log.Println("Starting zeroconf:", zeroConfName, zeroconfService, zeroconfPort)
+	defer log.Println("Zeroconf service terminated")
+
+	server, err := zeroconf.Register(
+		zeroConfName,
+		zeroconfService,
+		zeroconfDomain,
+		zeroconfPort,
+		[]string{"version=" + VERSION},
+		nil,
+	)
+	if err != nil {
+		log.Println("Zeroconf server error:", err)
+		return
+	}
+	defer server.Shutdown()
+
+	<-stop
+}
