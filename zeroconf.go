ae9e11d545f7fe62e535e5941a9308c6d404575b
Dan Sosedoff <dan.sosedoff@gmail.com>
2017-09-07 19:08:13 -0500

    Include hostname to zeroconf instance name

--- zeroconf.go
+++ zeroconf.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/grandcat/zeroconf"
 )
 
-const (
-	zeroConfName    = "app"
+var (
+	zeroConfName    = "Omxremote"
 	zeroconfService = "_omxremote._tcp"
 	zeroconfDomain  = "local."
 	zeroconfPort    = 8080
 )
 
 func startZeroConfAdvertisement(stop chan bool) {
+	hostname, err := os.Hostname()
+	if err == nil && hostname != "" {
+		zeroConfName = fmt.Sprintf("%s (%s)", zeroConfName, strings.Split(hostname, ".")[0])
+	}
+
 	log.Println("Starting zeroconf:", zeroConfName, zeroconfService, zeroconfPort)
 	defer log.Println("Zeroconf service terminated")
 
