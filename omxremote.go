87fde3c9858c6c3887f6a6ee55144d18ac8da544
Dan Sosedoff <dan.sosedoff@gmail.com>
2017-12-27 21:45:20 -0600

    Move file ops stuff into its own file

--- omxremote.go
+++ omxremote.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -72,75 +71,6 @@ var (
 	Frontend    bool           // Serve frontend app
 	stream      *Stream        // Current stream
 )
-
-// Returns true if specified file exists
-func fileExists(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil
-}
-
-// Scan given path for all directories and matching video files.
-// If nothing was found it will return an empty slice.
-func scanPath(path string) []FileEntry {
-	entries := make([]FileEntry, 0)
-
-	files, err := ioutil.ReadDir(path)
-	if err != nil {
-		return entries
-	}
-
-	for _, file := range files {
-		entry := FileEntry{
-			Filename: file.Name(),
-			IsDir:    file.IsDir(),
-		}
-
-		// Do not include non-video files in the list
-		if !file.IsDir() && !omxCanPlay(file.Name()) {
-			continue
-		}
-
-		entries = append(entries, entry)
-	}
-
-	return entries
-}
-
-// Convert media filename to regular title
-func fileToTitle(name string) string {
-	// Remove file extension from name
-	name = strings.Replace(name, filepath.Ext(name), "", 1)
-
-	// Replace all dots with white space
-	name = strings.Replace(name, ".", " ", -1)
-
-	// Replace parenteses and brackets
-	name = RegexBrackets.ReplaceAllString(name, "")
-
-	// Check if name has a typical torrent format: "name year format etc"
-	pos := RegexYear.FindStringIndex(name)
-	if len(pos) > 0 {
-		name = name[0:pos[0]]
-	} else {
-		// Check if its an episode, i.e. "S##E##"
-		pos = RegexEpisode.FindStringIndex(name)
-
-		if len(pos) > 0 {
-			name = name[0:pos[0]]
-		}
-	}
-
-	// Remove junk stuff
-	name = RegexJunk.ReplaceAllString(name, "")
-
-	// Remove any extra white space
-	name = RegexSpace.ReplaceAllString(name, "")
-
-	// Truncate space
-	name = strings.TrimSpace(name)
-
-	return name
-}
 
 // Determine the full path to omxplayer executable. Returns error if not found.
 func omxDetect() error {
